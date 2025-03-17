3e7d9c530fa3a4b30ba959507ab880669bbc3401
maksymbui <buimaksym@gmail.com>
2025-03-17 18:47:38 +1100

    Added comments and updated the code

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -8,118 +8,127 @@
 
 import Foundation
 
-enum Errors: Error{
-    case invalidInput(String)
-}
-
 class Calculator {
-    
-    /// For multi-step calculation, it's helpful to persist existing result
-    var currentResult = 0;
-    
-    /// Perform Addition
-    ///
-    /// - Author: Jacktator
-    /// - Parameters:
-    ///   - no1: First number
-    ///   - no2: Second number
-    /// - Returns: The addition result
-    ///
-    /// - Warning: The result may yield Int overflow.
-    /// - SeeAlso: https://developer.apple.com/documentation/swift/int/2884663-addingreportingoverflow
 
+    //Initialize result where we store final evaluation of all operations
+    var result: Int = 0
+    
+    //Create two separate arrays with valid operators to manage priority
+    let firstActions: [String] = ["x","/","%"]
+    let secondActions: [String] = ["+","-"]
+
+    //Function for addition
     func add(no1: Int, no2: Int) -> Int {
         return no1 + no2;
     }
 
-    func extract(no1: Int, no2: Int) -> Int{
+    //Function for substraction
+    func sub(no1: Int, no2: Int) -> Int{
         return no1 - no2    
     }
 
-    func divide(no1: Int, no2: Int) -> Int{
+    //Function for division
+    func div(no1: Int, no2: Int) -> Int{
         return no1 / no2;
     }
 
-    func multiply(no1: Int, no2: Int) -> Int{
+    //Function for multiplication
+    func mult(no1: Int, no2: Int) -> Int{
         return no1 * no2
     }
     
-    func module(no1: Int, no2: Int) -> Int{
+    //Function for modulo
+    func mod(no1: Int, no2: Int) -> Int{
         return no1 % no2
     }
-    
-    func calculate(args: [String]) throws -> String {
-        // Todo: Calculate Result from the arguments. Replace dummyResult with your actual result;
 
+    //Function for error handling
+    func errorHandling(array: [String]) throws{
+        //If no operators are included in the arguments return fatal error
+        if array.count == 0{
+            throw fatalError()
+        }
+
+        //If invalid operators are included in the arguments return fatal error
+        for op in array{
+            if !firstActions.contains(op) && !secondActions.contains(op){
+                throw fatalError()
+            }
+        }
+    }
+    
+    //Main function to calculate result
+    func calculate(args: [String]) throws -> String {
+        //Transfer arguments into a new variable 
         let newArray: [String] = args
-        let firstActions: [String] = ["x","/","%"]
-        let secondActions: [String] = ["+","-"]
-        var result: Int = 0
-        
+
+        //If only one argument was provided and can be converted to String, return this element        
         if args.count == 1{
             if let number = Int(args[0]){
                 return "\(number)"
             }
         }
-    
+
+        //Divide elemets of array into separate arrays of numbers and operators
         var numbers: [Int] = []
         var operators: [String] = []
-        
         for element in newArray{
-            if let num = Int(element){
-                numbers.append(num)
+            //Loop through each given argument (Array of String)
+            if let num = Int(element){ //Try to convert String to integer
+                numbers.append(num) //If successful, add element to array of numbers
             } else {
-                operators.append(element)
+                operators.append(element)//else, add element to array of operators
             }
         }
 
-        if operators.count == 0{
-            throw fatalError()
-        }
-
-        for op in operators{
-            if !firstActions.contains(op) && !secondActions.contains(op){
-                throw fatalError()
-            }
-        }
-                
-        if operators.count != 0{
-            var currIndex: Int = 0
-            while currIndex < operators.count{
-                let op = operators[currIndex]
-                if firstActions.contains(op){
-                    let firstNum = numbers[currIndex]
-                    let secondNum = numbers[currIndex+1]
-                    var newNum: Int = 0
-                    switch op{
-                    case "x":
-                        newNum = multiply(no1:firstNum,no2: secondNum)
-                    case "/":
-                        newNum = divide(no1: firstNum, no2: secondNum)
-                    case "%":
-                        newNum = module(no1: firstNum, no2: secondNum)
-                    default:
-                        throw Errors.invalidInput("exit with nonzero status on invalid input: calc \(args)")
-                    }
-                    numbers[currIndex] = newNum
-                    numbers.remove(at: currIndex+1)
-                    operators.remove(at: currIndex)
-                } else {
-                    currIndex += 1
+        //Review given arguments and handle errors
+        try errorHandling(array: operators)
+    
+        //Do multiplication, division and modulo first
+        //Go through all given operators and look for operators like [* / %]
+        var currIndex: Int = 0
+        while currIndex < operators.count{
+            let op = operators[currIndex]
+            if firstActions.contains(op){
+                let firstNum = numbers[currIndex]
+                let secondNum = numbers[currIndex+1]
+                var newNum: Int = 0 //Store result of operation
+                //Run function depending on given operator
+                switch op{
+                case "x":
+                    newNum = mult(no1:firstNum,no2: secondNum)
+                case "/":
+                    newNum = div(no1: firstNum, no2: secondNum)
+                case "%":
+                    newNum = mod(no1: firstNum, no2: secondNum)
+                default:
+                    throw fatalError()//If operator is invalid, throw error
                 }
+                //Put the result of the operation inside our numbers array
+                //Remove numbers and operator from arrays, to store only remaining numbers and operators to process
+                numbers[currIndex] = newNum
+                numbers.remove(at: currIndex+1)
+                operators.remove(at: currIndex)
+            } else {
+                //Go to next operator
+                currIndex += 1
             }
-            result = numbers[0]
         }
 
+        //Assign result to fist number of the array with remaining numbers
+        result = numbers[0]
+        
+        //If after finishing first actions [x,/,%] there are still operators left, they must be part of second action [+,-].
+        //Evaluate remaining operations
         if operators.count != 0{
             for i in 0..<operators.count{
                 switch operators[i]{
                 case "+":
-                    result += numbers[i+1]
+                    result = add(no1: result, no2: numbers[i+1])
                 case "-":
-                    result -= numbers[i+1]
+                    result = sub(no1: result, no2: numbers[i+1])
                 default:
-                    break;
+                    throw fatalError()
                 }
             }
         }
